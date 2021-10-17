3ab2ba7f561c6c5af8646b16f1b104afb56cb581
Fábio <fabio.dev07@gail.com>
2021-10-17 14:05:17 -0300

    one page demo

---
+++ script/script.js
@@ -0,0 +1,10 @@
+        const sr = ScrollReveal({
+                origin: 'top',
+                distance: '50px',
+                duration: 800,
+                reset: true
+            });
+    
+            ScrollReveal().reveal('.container-header', { delay: 200 });
+            ScrollReveal().reveal('.container-section', { delay: 400 });
+            ScrollReveal().reveal('container-contato', { delay: 500 });
